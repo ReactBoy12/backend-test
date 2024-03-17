40cddf81cf40baa206b988287f696ca19505bca5
shailesh gore <shaileshgore1998@gmail.com>
2024-03-17 11:39:40 +0530

    first initialization

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [product, setProduct] = useState([]);
+
+  useEffect(()=>{
+    axios.get('/api/jokes')
+    .then((response)=>{
+      setProduct(response.data);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  })
+  return (
+    <>
+     <h1>Hello Frontend</h1>
+     <div>Products : {product.length}</div>
+     {
+      product.map((prod,i)=>{
+          return(<div>
+            <h1>{prod.id}</h1>
+            <p>{prod.title}</p>
+            <h3>{prod.price}</h3>
+          </div>)
+      })
+     }
+    </>
+  )
+}
+
+export default App
