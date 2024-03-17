c4353d42c98c616345e367e8356a25d1ab809362
shailesh gore <shaileshgore1998@gmail.com>
2024-03-17 12:01:57 +0530

    changes in the frontend file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import axios from 'axios'
 
 function App() {
   const [product, setProduct] = useState([]);
+  console.log("get ready for the memorial branch")
 
   useEffect(()=>{
     axios.get('/api/jokes')
